05b2ae07435869622e0cabcd9421463fd5d73df7
PlantDefenseMetabolism <naake@stud.uni-heidelberg.de>
2015-10-23 11:57:53 +0200

    add functionality 2

--- scriptConversionMod.R
+++ scriptConversionMod.R
@@ -1,20 +1,23 @@
+## Data from Halle
+spectra_0_less <- read.csv("~/Documents/University/Master/HiWi/20158251022_spectra_0_less.msp", 
+                           comment.char="#")
+rawmatrix_0_less <- read.delim("~/Documents/University/Master/HiWi/20158251022_rawmatrix_0_less.txt")
 
 ## PNAS article
 ## sd01
-sd01_outputXCMS <- read.csv("sd01_outputXCMS.csv", header=FALSE, sep=";")
+sd01_outputXCMS <- read.csv("~/Documents/University/Master/HiWi/sd01_outputXCMS.csv", header=FALSE, sep=";")
 colnames(sd01_outputXCMS) <- as.matrix(sd01_outputXCMS[3,])[1,]
 sd01_outputXCMS <- sd01_outputXCMS[-c(1:3),]
-sd01_FoldChangePvalue <- read.csv("sd01_FoldchangePvalue.csv", sep=";")
+sd01_FoldChangePvalue <- read.csv("~/Documents/University/Master/HiWi/sd01_FoldchangePvalue.csv", sep=";")
 ## sd02
-sd02_similarityMatrix <- read.csv("sd02_similarityMatrix.csv", sep=";")
+sd02_similarityMatrix <- read.csv("~/Documents/University/Master/HiWi/sd02_similarityMatrix.csv", sep=";")
 rownames(sd02_similarityMatrix) <- sd02_similarityMatrix[,1]
 sd02_similarityMatrix <- sd02_similarityMatrix[,-1]
-sd02_deconvoluted <- read.csv("sd02_deconvolutedidMSMS.csv", sep=";")
+sd02_deconvoluted <- read.csv("~/Documents/University/Master/HiWi/sd02_deconvolutedidMSMS.csv", sep=";")
 
 
-# Camera 
-## msp file
-# 1_ every one that has one identifier has one spectrum, take average as rt, leave adductionname empty
+
+## 1) convert sd02_deconvoluted to .msp format
 
 ## identify precursor mz
 precursor <- sd02_deconvoluted[,4]
@@ -65,5 +68,83 @@ setwd("Documents/University/Master/HiWi/")
 ## write finalMSP to .msp
 write.table(finalMSP, file = "idMSMStoMSP.msp", sep=" ", dec=".",
     row.names=FALSE, col.names=FALSE,quote=FALSE)
+
+################################################################################
+
+## 2) convert sd02_deconvoluted precursor ions and sd01_outputXCMS to a format
+## as in ..._rawmatrix_0_less.txt by allocating precursor ions to canditate
+## m/z values based on minimal distance of m/z
+
+mz <- uniquePreMZ_cut
+indL <- list(NULL)
+offset <- 0.5
+
+minMZ <- as.character(sd01_outputXCMS[,3])
+minMZ <- as.numeric(minMZ)
+maxMZ <- as.character(sd01_outputXCMS[,4])
+maxMZ <- as.numeric(maxMZ)
+
+## preselection of possible entries
+while (any(unlist(lapply(indL, length)) == 0)) {
+    offset <- offset + 0.01
+    for (i in 1:length(uniquePreMZ_cut)) {
+        ind <- which( (minMZ-offset) <= mz[i] & mz[i] <= (maxMZ+offset) )
+        indL[[i]] <- ind
+    }
+}
+
+## find from these the preselection the entry with the smallest m/z deviation
+## to the precursor mz
+
+## create finalCluster, which is the data.frame to store data
+finalCluster <- matrix(nrow = length(mz), ncol = (5 + 183 + 2))
+colnames(finalCluster) <- c("Average Rt(min)", "Average mz", "Metabolite Name",
+    "Adduct ion name", "Spectrum reference file name", as.character(1:183), 
+    "Quality RT", "minimum deviation m/z")
+finalCluster <- as.data.frame(finalCluster)
+
+for (i in 1:length(mz)) {
+    mzXCMS <- sd01_outputXCMS[indL[[i]],"mz"]
+    mzXCMS <- as.character(mzXCMS)
+    mzXCMS <- as.numeric(mzXCMS)
+    devMZ <- abs(mzXCMS - mz[i])
+    indMinmz <- which.min(devMZ)
+    XCMS <- sd01_outputXCMS[indL[[i]],][indMinmz,]
+
+    ## check if rt from deconvoluted is in range of rtmin to rtmax of sd01_outputXCMS
+    ## and assign quality mark
+    rtDeconvoluted <- sd02_deconvoluted[i,"rt"]
+    rtminXCMS <- XCMS[,"rtmin"]
+    rtminXCMS <- as.character(rtminXCMS)
+    rtminXCMS <- as.numeric(rtminXCMS)
+
+    rtmaxXCMS <- XCMS[,"rtmax"]
+    rtmaxXCMS <- as.character(rtmaxXCMS)
+    rtmaxXCMS <- as.numeric(rtmaxXCMS)
+
+    qualityRT <- rtminXCMS <= rtDeconvoluted & rtDeconvoluted <= rtmaxXCMS
+    if (qualityRT) {quality <- 1} else {quality <- 0}
+    
+    indRT <- which(uniquePreMZ[i] == precursor)   
+    meanFakeRT <- mean(sd02_deconvoluted[indRT,"rt"])
+    
+    entry <- matrix(0, nrow = 1, ncol = ncol(finalCluster))
+    colnames(entry) <- colnames(finalCluster)
+    entry[, "Average Rt(min)"] <- meanFakeRT
+    entry[, "Average mz"] <- mz[i]
+    entry[, "Metabolite Name"] <- "Unknown"
+    entry[, "Adduct ion name"] <- XCMS[, "adduct"]
+    entry[, "Spectrum reference file name"] <- "Unknown"
+    
+    x <- XCMS[,which(colnames(XCMS) == "1"):which(colnames(XCMS) == "183")]
+    x <- as.matrix(x)
+    x <- as.vector(x)
+    entry[, which(colnames(entry)=="1"):which(colnames(entry)=="183")] <- x
+            
+    entry[, "Quality RT"] <- quality
+    entry[, "minimum deviation m/z"] <- min(devMZ)
+    
+    finalCluster[i, ] <- entry
+}
 
 
