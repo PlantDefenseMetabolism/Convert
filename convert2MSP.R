f3ed5e4f8139619774fd91756a2c3e4295479b76
PlantDefenseMetabolism <naake@stud.uni-heidelberg.de>
2015-11-26 14:44:43 +0100

    write function convert2MSP

---
+++ convert2MSP.R
@@ -0,0 +1,52 @@
+## function to convert mm into MSP format
+
+## mm needs to be in the format: 
+## 1st column: mz, 2nd column: rt, 3rd column: intensity, 
+## 4th column: pcgroup_precursorMZ, (5th column: PrecursorMZ)
+## concerning 4th column, split is the pattern which separates elements and precursor mz
+## splitInd is the position of the precursor mz concerning separatation by split pattern
+
+convert2MSP <- function (mm, split = "_", splitInd = 1) {
+    precursor <- mm[,4]
+    precursor <- as.character(precursor)
+    splitPrecursor <- strsplit(precursor, split=split) ## split precursors
+    splitPrecursor <- lapply(splitPrecursor,"[", splitInd)
+    PrecursorMZ <- unlist(splitPrecursor)
+    lenPreMZ <- length(PrecursorMZ)
+    
+    ## add PrecursorMZ to deconvoluted idMSMS
+    mm <- cbind(mm, PrecursorMZ)
+    
+    ## change character to numeric
+    PrecursorMZ <- as.numeric(PrecursorMZ)
+    uniquePreMZ <- unique(precursor)
+    lenUniquePreMZ <- length(uniquePreMZ)
+    uniquePreMZ_cut <- unique(PrecursorMZ)
+    
+    ## create data frame for MSP file
+    finalMSP <- matrix(data = NA, nrow = 7 * lenUniquePreMZ + dim(mm)[1], ncol = 2) ## 7 new entries + all fragment ion entries
+    finalMSP <- as.data.frame(finalMSP)
+    
+    ## write to data frame
+    for (i in 1:lenUniquePreMZ) {
+        ind <- which(uniquePreMZ[i] == precursor)    
+        entry <- rbind(
+            c("NAME: ", "Unknown"),
+            c("RETENTIONTIME: ", mean(mm[ind,"rt"])),
+            c("PRECURSORMZ: ", uniquePreMZ_cut[i]),
+            c("METABOLITENAME: ", "Unknown"),
+            c("ADDUCTIONNAME: ", "Unknown"),
+            c("Num Peaks: ", length(ind)),
+            mm[ind,c(1,3)],
+            c(" ", " ")
+        )
+        entry <- as.matrix(entry)
+        ## determine first empty line
+        newstart <- which(is.na(finalMSP[,1]))[1]
+        ## determine last line to write to
+        newend <- newstart + dim(entry)[1] - 1
+        finalMSP[newstart:newend,] <- entry
+    }
+    
+    return(finalMSP)
+}
--- scriptConversion.R
+++ scriptConversion.R
@@ -15,59 +15,31 @@ rownames(sd02_similarityMatrix) <- sd02_similarityMatrix[,1]
 sd02_similarityMatrix <- sd02_similarityMatrix[,-1]
 sd02_deconvoluted <- read.csv("sd02_deconvolutedidMSMS.csv", sep=";")
 
-
-
 ## 1) convert sd02_deconvoluted to .msp format
 
 ## identify precursor mz
-precursor <- sd02_deconvoluted[,4]
-precursor <- as.character(precursor)
-splitPrecursor <- strsplit(precursor, split=" _ ") ## split precursors
-splitPrecursor <- unlist(splitPrecursor)
-
-lenSplitPrecursor <- length(splitPrecursor)
-PrecursorMZ <- splitPrecursor[seq(2, lenSplitPrecursor, 2)]
-lenPreMZ <- length(PrecursorMZ)
-
-## add PrecursorMZ to deconvoluted idMSMS
-sd02_deconvoluted <- cbind(sd02_deconvoluted, PrecursorMZ)
-
-## change character to numeric
-PrecursorMZ <- as.numeric(PrecursorMZ)
-## PrecursorMZ <- sort(PrecursorMZ) ## ??
-uniquePreMZ <- unique(precursor)
-lenUniquePreMZ <- length(uniquePreMZ)
-uniquePreMZ_cut <- unique(PrecursorMZ)
-
-## create data frame for MSP file
-finalMSP <- matrix(data = NA, nrow = 7 * lenUniquePreMZ + dim(sd02_deconvoluted)[1], ncol = 2) ## 7 new entries + all fragment ion entries
-finalMSP <- as.data.frame(finalMSP)
-
-## write to data frame
-for (i in 1:lenUniquePreMZ) {
-    ind <- which(uniquePreMZ[i] == precursor)    
-    entry <- rbind(
-        c("NAME: ", "Unknown"),
-        c("RETENTIONTIME: ", mean(sd02_deconvoluted[ind,"rt"])),
-        c("PRECURSORMZ: ", uniquePreMZ_cut[i]),
-        c("METABOLITENAME: ", "Unknown"),
-        c("ADDUCTIONNAME: ", "Unknown"),
-        c("Num Peaks: ", length(ind)),
-        sd02_deconvoluted[ind,c(1,3)],
-        c(" ", " ")
-    )
-    entry <- as.matrix(entry)
-    ## determine first empty line
-    newstart <- which(is.na(finalMSP[,1]))[1]
-    ## determine last line to write to
-    newend <- newstart + dim(entry)[1] - 1
-    finalMSP[newstart:newend,] <- entry
-}
-
+finalMSP <- convert2MSP(sd02_deconvoluted, split = " _ ", splitInd = 2)
 
 ## write finalMSP to .msp
 write.table(finalMSP, file = "idMSMStoMSP.msp", sep=" ", dec=".",
-    row.names=FALSE, col.names=FALSE,quote=FALSE)
+            row.names=FALSE, col.names=FALSE,quote=FALSE)
+
+
+############ tissue project
+## 1) convert idMSMStissueproject.txt to .msp format
+tissue <- read.csv("idMSMStissueproject.txt",sep="\t")
+## change tissue to format that it is compatible with writeMSP sd02_deconvoluted
+## 1st column: mz, 2nd column: rt, 3rd column: intensity, 
+## 4th column: pcgroup_precursorMZ, (5th column: PrecursorMZ)
+newtissue <- tissue
+newtissue[,2] <- tissue[,3]; colnames(newtissue)[2] <- colnames(tissue)[3]
+newtissue[,3] <- tissue[,2]; colnames(newtissue)[3] <- colnames(tissue)[2]
+newtissue[,4] <- tissue[,7]; colnames(newtissue)[4] <- colnames(tissue)[7]
+newtissue <- newtissue[,1:4]
+tissue <- newtissue
+
+finalMSP <- convert2MSP(tissue, split = "_", splitInd=1)
+
 
 ################################################################################
 
@@ -199,7 +171,7 @@ write.table(finalCluster, file = "finalCluster.csv", sep=";", dec=".",
     row.names=FALSE, col.names=TRUE,quote=FALSE)
 
 ################################################################################
-## create table with same fragments (binning)
+## 3) create table with same fragments (binning)
 msp <- finalMSP
 IndPrecMZ <- which(msp[,1] == "PRECURSORMZ: ")
 precmz <- msp[IndPrecMZ,2]
@@ -359,32 +331,95 @@ NDP <- function(mat, row1=1, row2=2, m = 0.5, n = 2) {
     len <- dim(mat)[2]
     mass <- colnames(mat)
     mass <- as.numeric(mass)
-    WS1 <- numeric(len)
-    WS2 <- numeric(len)
+    ##WS1 <- numeric(len)
+    ##WS2 <- numeric(len)
     S1 <- as.numeric(mat[row1,])
     S2 <- as.numeric(mat[row2,])
-    for (i in 1:len) { ## formula according to Li et al 2015, PNAS
-        WS1[i] <- ( S1[i] ) ^ m * ( mass[i] ) ^ n
-        WS2[i] <- ( S2[i] ) ^ m * ( mass[i] ) ^ n
-    }
+    ##for (i in 1:len) { ## formula according to Li et al 2015, PNAS
+    ##    WS1[i] <- ( S1[i] ) ^ m * ( mass[i] ) ^ n ## WS1 <- S1 ^ m * mass ^ n
+    ##    WS2[i] <- ( S2[i] ) ^ m * ( mass[i] ) ^ n ## WS2 <- S2 ^ m * mass ^ n
+    ##}
+    WS1 <- S1 ^ m * mass ^ n
+    WS2 <- S2 ^ m * mass ^ n
     NDP <- ( sum(WS1 * WS2) ) ^ 2 / (sum(WS1 ^ 2 ) * sum(WS2 ^ 2))
     return(NDP)
 }
+
 
 ## create similarity matrix which contains pairwise similarity measure NDP
 similarityMatrix <- matrix(0, nrow = length(precmz), ncol = length(precmz))
 colnames(similarityMatrix) <- rownames(similarityMatrix) <- paste(precmz, rt, sep="/")
 
-similarityMatrix <- similarityMatrix
-for (i in 1:dim(similarityMatrix)[1]) {
-    for (j in 1:dim(similarityMatrix)[1]) {
-        similarityMatrix[i,j] <- NDP(mm, row1=i, row2=j)
+n <- dim(similarityMatrix)[1]
+## write to similarity matrix similarit measure
+for (i in 1:n) {
+    for (j in 1:n) {
+        if (i <= j) {
+            similarityMatrix[j,i] <- similarityMatrix[i,j] <- NDP(mm, row1=i, row2=j)
+        }
     }
 }
 
 ## Clustering
+library(amap)
 hClustMSP <- hcluster(similarityMatrix, method = "spearman")
-plot(hClustMSP, labels=FALSE)
-##data.frame # rownames unique identifier, colnames = binned m/z
-## entries 0 falls keine Ähnlichkeit, oder relative Intensität
+plot(hClustMSP, labels = FALSE)
+colnames(similarityMatrix)[hClustMSP$order]
+# hClustMSP2 <- hcluster(similarityMatrix[1:5,1:5], method = "spearman")
+# plot(hClustMSP2, labels = FALSE)
+# plot(hClustMSP2)
 
+## order newSim according to order of clustering
+
+colnames(similarityMatrix)
+hClustMSP$order
+newSim <- matrix(data=NA, nrow = dim(similarityMatrix)[1], ncol = dim(similarityMatrix)[1])
+newColnames <- colnames(similarityMatrix)[hClustMSP$order]
+colnames(newSim) <- rownames(newSim) <- newColnames
+
+for (i in 1:dim(similarityMatrix)[1]) {
+    newValues <- similarityMatrix[hClustMSP$order, hClustMSP$order[i]] 
+    if (all(names(newSim[,i]) == names(newValues))) {
+        newSim[,i] <- newValues
+    } else {print("error");break}
+}
+
+write.csv(newSim, "orderedSimilarityMatrix.csv", sep=";", dec=".")
+##
+## circos
+NL <- c("CH2", "CH4", "NH3", "H2O", "K+toNH4+", "Na+toH+", "C2H2", 
+        "CO", "C2H4", "CH3N", "CH2O", "CH5N", "S", "H2S", 
+        "K+toH+", "C2H2O", "C3H6", "CHNO", "CO2", "CH2O2", "C4H8",
+        "C3H9N", "C2H4O2", "CH4N2O", "SO2", "C5H8", "C3H6O2", "C6H6",
+        "SO3", "C3H2O3", "C4H8O2", "C4H12N2", "H2SO4", "H3PO4", "C5H10O2",
+        "C3H4O4", "C6H12O2", "C2H5O4P", "C5H8O4", "C7H19N3", "C6H10O4", "C6H10O5",
+        "C6H12O5", "C6H8O6", "C6H12O6", "C6H10O7", "C8H12O6", "C11H10O4", "C10H15N3O6S", 
+        "C10H17N3O6S", "C12H20O10", "C12H22O11")
+
+mzNL <- c(14.0157, 16.0313, 17.0265, 18.0106, 20.9293, 21.9819, 26.0157, 
+        27.9949, 28.0313, 29.0266, 30.0106, 31.0422, 31.9721, 33.9877,
+        37.9559, 42.0106, 42.0470, 43.0058, 43.9898, 46.0055, 56.0626,
+        59.0735, 60.0211, 60.0324, 63.9619, 68.0626, 74.0368, 78.0470,
+        79.9568, 86.0004, 88.0517, 88.1000, 97.9674, 97.9769, 102.0618,
+        104.0110, 116.0861, 123.9926, 132.0423, 145.1579, 146.0579, 162.0528,
+        164.0685, 176.0321, 180.0634, 194.0427, 204.0655, 206.0579, 305.0682,
+        307.0838, 324.1057, 342.1162)
+nl <- matrix(mzNL, nrow=1, ncol = length(NL))
+colnames(nl) <- NL
+
+x <- mm[1,]
+frag <- names(x)
+frag <- as.numeric(frag)
+precursor <- rownames(mm)[1]
+precursor <- strsplit(precursor, "/")[[1]]
+precursor <- as.numeric(precursor[1])
+precursorInd <- which.min(abs(frag-precursor))
+PRECURSOR <- x[precursorInd]
+PRECURSORiMZ <- as.numeric(names(PRECURSOR))
+PRECURSORiIntensity <- as.numeric(PRECURSOR)
+indsFrag <- which(as.numeric(x) != 0)
+
+FRAGj <- x[indsFrag[2]]
+FRAGjMZ <- as.numeric(names(FRAGj))
+FRAGjIntensity <- as.numeric(FRAGj)
+FRAGjMZ - PRECURSORiMZ
